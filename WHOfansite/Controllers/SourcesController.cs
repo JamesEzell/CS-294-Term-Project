52c995409adad99229bb0c4444e21679b5f2ae8e
James Ezell <Ezell_JamesP40@outlook.com>
2018-10-14 09:20:08 -0700

    10/13 Homework

--- WHOfansite/Controllers/SourcesController.cs
+++ WHOfansite/Controllers/SourcesController.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WHOfansite.Models;
 
 namespace WHOfansite.Controllers
 {
     public class SourcesController : Controller
     {
-        public IActionResult Index()
+        Book book;
+        
+        public SourcesController()
         {
-            return View();
+            List<Book> books = new List<Book>();
+            books.Add(new Book()
+            {
+                Author = "Marc Platt",
+                Title = "Lungbarrow",
+                PubDate = new DateTime(1997, 1, 1)
+            });
+            
+            books.Add(new Book()
+            {
+                Author = "Steve Lyons",
+                Title = "Head Games",
+                PubDate = new DateTime(1997, 1, 1)
+            });
+            
         }
 
         public IActionResult Books()
         {
             ViewData["Message"] = "Books and print media about the person or persons";
 
-            return View();
+            List<Book> books = Book.Books;
+            books.Sort((b1, b2) => b1.Title.CompareTo(b2.Title));
+            return View(books);
         }
 
         public IActionResult Links()
@@ -25,6 +44,18 @@ namespace WHOfansite.Controllers
             ViewData["Message"] = "Links to online media about a person or persons";
 
             return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult AddBook(string title,
+                                              string author, string pubDate)
+        {
+            book = new Book();
+            book.Author = author;
+            book.Title = title;
+            book.PubDate = DateTime.Parse(pubDate);
+            
+            return RedirectToAction("Books");
         }
     }
 }
---
+++ WHOfansite/Models/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WHOfansite.Models
+{
+    public class Book : IEnumerable<Book>
+    {
+        private static List<Book> books = new List<Book>();
+
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public DateTime PubDate { get; set; }
+
+        public static List<Book> Books { get { return books; } }
+
+        public interface IEnumerable<Book>
+        {
+            IEnumerator<Book> GetEnumerator();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (Book b in books)
+            {
+                yield return b;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
