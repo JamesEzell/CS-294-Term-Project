5ceee8bbb4ea848adfe9d0c3bc9a67b7bc62588e
James Ezell <Ezell_JamesP40@outlook.com>
2018-10-16 16:58:56 -0700

    10/16 Books and Links now displayed

--- WHOfansite/Controllers/SourcesController.cs
+++ WHOfansite/Controllers/SourcesController.cs
@@ -9,58 +9,59 @@ namespace WHOfansite.Controllers
 {
     public class SourcesController : Controller
     {
-        //add these lists to the repository
-        Book books;
-        Link links;
         
         public SourcesController()
         {
-            List<Book> books = new List<Book>();
-            books.Add(new Book()
+            Book book;
+            Link link;
+
+            book = (new Book()
             {
                 Author = "Marc Platt",
                 Title = "Lungbarrow",
                 PubDate = new DateTime(1997, 1, 1)
             });
+            Repository.AddBook(book);
             
-            books.Add(new Book()
+            book = (new Book()
             {
                 Author = "Steve Lyons",
                 Title = "Head Games",
                 PubDate = new DateTime(1997, 1, 1)
             });
+            Repository.AddBook(book);
 
-            List<Link> links = new List<Link>();
-            links.Add(new Link()
+            link = (new Link()
             {
                 PageTitle = "The TARDIS Library",
                 PageUrl = "http://www.timelash.com/tardis/",
                 PageInfo = "Online hub for information and sales of Doctor Who merchandise in very medium imaginable",
             });
+            Repository.AddLink(link);
 
-            links.Add(new Link()
+            link = (new Link()
             {
                 PageTitle = "List of Doctor Who Serials from 1963-1989",
                 PageUrl = "https://en.wikipedia.org/wiki/List_of_Doctor_Who_episodes_(1963%E2%80%931989)",
                 PageInfo = "Every serial from the show's first run, because it's the only run that matters",
             });
-
+            Repository.AddLink(link);
         }
 
         public IActionResult Books()
         {
             ViewData["Message"] = "Books and print media about the person or persons";
 
-            List<Book> books = ;
+            List<Book> books = Repository.Books;
             books.Sort((b1, b2) => b1.Title.CompareTo(b2.Title));
             return View(books);
         }
 
-        public IActionResult OnlineLinks()
+        public IActionResult Links()
         {
             ViewData["Message"] = "Links to online media about a person or persons";
 
-            List<Link> pageLinks = ;
+            List<Link> pageLinks = Repository.Links;
             pageLinks.Sort((b1, b2) => b1.PageTitle.CompareTo(b2.PageTitle));
             return View(pageLinks);
 
--- WHOfansite/Models/Repository.cs
+++ WHOfansite/Models/Repository.cs
@@ -13,7 +13,12 @@ namespace WHOfansite.Models
         private static List<Comment> comments = new List<Comment>();
         public static List<Comment> Comments { get { return comments; } }
 
+        private static List<Book> books = new List<Book>();
+        public static List<Book> Books { get { return books; } }
 
+        private static List<Link> links = new List<Link>();
+        public static List<Link> Links { get { return links; } }
+        
 
         public static void AddSubmission(Story submission)
         {
@@ -23,6 +28,16 @@ namespace WHOfansite.Models
         public static void AddComment(Comment comment)
         {
             comments.Add(comment);
+        }
+
+        public static void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public static void AddLink(Link link)
+        {
+            links.Add(link);
         }
     }
 }
