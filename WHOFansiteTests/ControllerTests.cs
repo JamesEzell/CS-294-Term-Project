c1165804a2aebc7035c81aa7d29c56325df301fd
James Ezell <Ezell_JamesP40@outlook.com>
2018-11-10 17:48:22 -0800

    almost there...

--- WHOFansiteTests/ControllerTests.cs
+++ WHOFansiteTests/ControllerTests.cs
@@ -1,4 +1,6 @@
 using Xunit;
+using WHOfansite.Models;
+using WHOfansite.Repositories;
 
 namespace WHOFansiteTests
 {
---
+++ WHOfansite/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WHOfansite.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+
+        public DbSet<Story> Submissions { get; set; }
+    }
+}
