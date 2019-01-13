d85ff7d68ae4d1c4cbcbd8170ebba4ee7c9a3445
James Ezell <jamespaulezell@gmail.com>
2019-01-12 20:45:56 -0800

    Added CreateUser async task 01122045

---
+++ WHOfansite/Controllers/AdminController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WHOfansite.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WHOfansite.Controllers
+{
+    public class AuthController : Controller
+    {
+        private UserManager<User> userManager;
+
+        public AuthController(UserManager<User> um)
+        {
+            userManager = um;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult ShowAccounts()
+        {
+            return View(userManager.Users);
+        }
+
+        public ViewResult CreateUser() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                IdentityResult result
+                    = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            User user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View("Index", userManager.Users);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
